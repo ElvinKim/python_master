e037c01ec8ab35c4dbb57f440cfc982cfc03541a
ElvinKim <bonwho09@gmail.com>
2016-12-12 18:43:57 +0900

    [add] descriptor study - bulkfood v4b

---
+++ fluent_python/descriptor/bulkfood_v4b.py
@@ -0,0 +1,43 @@
+class Quantity(object):
+    __counter = 0
+
+    def __init__(self):
+        cls = self.__class__
+        prefix = cls.__name__
+        index = cls.__counter
+        self.storage_name = '_{}#{}'.format(prefix, index)
+        cls.__counter += 1
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        if value > 0:
+            setattr(instance, self.storage_name, value)
+        else:
+            raise ValueError("value must be > 0")
+
+
+class LineItem(object):
+
+    weight = Quantity()
+    price = Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+if __name__ == "__main__":
+
+    apple = LineItem("apple", 100, 10)
+    print(getattr(apple, '_Quantity#0'))
+    print(getattr(apple, '_Quantity#1'))
+
